a3e9ccaa1a7b237ad0fd68bb4cc4469b40331357
Lara Stucki <lara.stucki@hotmail.com>
2026-05-04 11:56:20 +0200

    makefile.R

--- R/makefile.R
+++ R/makefile.R
@@ -8,48 +8,28 @@ library("lubridate")
 library("ISOweek")
 library("tidyverse")
 library("data.table")
-library("readxl")
 library("rio")
-
 
 
 # 1. Yearly population grids (HPC) ----
 source("R/1_population.R") 
 
-# 2. NUTS codes ----
+# 2. NUTS codes (HPC) ----
 source("R/2_nuts.R")
 
-# 3a. Mortality data daily counts----
+# 3. Mortality data daily counts (HPC) ----
 source("R/3_mortality.R")
 
-# 3b. Mortality data weekly counts ----
-source("R/3_mortality_weekly.R")
-
-# 4a. Merge exposures, population, NUTS codes - daily (HPC) ----
+# 4. Merge exposures, population, NUTS codes - daily (HPC) ----
 source("R/4_assemble.R")
-
-# 4b. Merge exposures, population, NUTS codes - weekly (HPC) ----
-source("R/4_assemble_weekly.R")
 
 # 5. Exposure and fire risk subindicator by area (HPC) ----
 source("R/5_exposure.R")
 
-# 6a. Attributable mortality subinidicator _ daily HIA (HPC) ----
+# 6. Attributable mortality subinidicator _ daily HIA (HPC) ----
 source("R/6_HIA.R")
 
-# 6b. Attributable mortality subinidicator - weekly HIA (HPC) ----
-source("R/6_HIA_weekly.R")
-
-# 6c. Attributable mortality subinidicator (HPC) ----
-source("R/6_HIA_sens_lag0-1.R")
-
-# 6d. Attributable mortality subinidicator (HPC) ----
-source("R/6_HIA_sens_lag0-7.R")
-
-# 6e. Attributable mortality subinidicator (HPC) ----
-source("R/6_HIA_sens_RR_totalpm25.R")
-
-# 7. FWI fire risk subindicator ----
+# 7. FWI fire risk subindicator (HPC) ----
 source("R/7_FWI.R")
 
 # 8. Figures and tables ----
